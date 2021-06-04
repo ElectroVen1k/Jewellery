862789f849b41a92e1822972d51bdac49c5a57b4
ElectroVen1k <aleck1998@yandex.ru>
2021-06-05 00:02:26 +0400

    добавляет шапку

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,30 @@
 (function() {
+  var headerElement = document.querySelector('.header');
+  var menuToggler = document.querySelector('.header__menu-toggler');
+  var body = document.querySelector('.body');
 
+
+  window.onload = function() {
+    headerElement.classList.remove('header--opened');
+    headerElement.classList.remove('header--no-js');
+  };
+
+  var openMenu = function () {
+    headerElement.classList.add('header--opened');
+    body.classList.add('body--noscroll');
+  };
+
+
+  var closeMenu = function () {
+    headerElement.classList.remove('header--opened');
+    body.classList.remove('body--noscroll');
+  }
+
+  menuToggler.addEventListener('click',  () => {
+    if (headerElement.classList.contains('header--opened')){
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
 }());
