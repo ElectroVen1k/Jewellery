32ed986ed30ccb941ae83f2f89250da24b677cb5
ElectroVen1k <aleck1998@yandex.ru>
2021-06-09 00:12:24 +0400

    Добавляет главную страницу

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,13 @@
 (function() {
+  //Menu
   var headerElement = document.querySelector('.header');
   var menuToggler = document.querySelector('.header__menu-toggler');
   var body = document.querySelector('.body');
 
-
   window.onload = function() {
     headerElement.classList.remove('header--opened');
     headerElement.classList.remove('header--no-js');
+    closeAllAnswers();
   };
 
   var openMenu = function () {
@@ -27,4 +28,51 @@
       openMenu();
     }
   });
+
+  //Q&A
+
+  var questionsBlock = document.querySelectorAll('.questions__item');
+
+  var openAnswer = function (block) {
+    block.classList.add('questions__item--opened');
+  }
+
+  var closeAnswer = function (block) {
+    block.classList.remove('questions__item--opened');
+  }
+
+  var questionsToggle = function (block) {
+    block.addEventListener('click', function (evt) {
+      if (block.classList.contains('questions__item--opened')) {
+        closeAnswer(block);
+      } else {
+        openAnswer(block);
+      }
+    });
+  }
+
+  var onQuestionsBlockPressEnter = function (block) {
+    return function(evt) {
+      if (evt.key === 'Enter') {
+        if (block.classList.contains('questions__item--opened')) {
+          closeAnswer(block);
+        } else {
+          openAnswer(block);
+        }
+      }
+    }
+  };
+
+  var closeAllAnswers = function () {
+    for(var block of questionsBlock) {
+      block.classList.remove('questions__item--opened');
+    }
+  }
+
+
+  for (var block of questionsBlock) {
+    block.addEventListener('click', questionsToggle(block));
+    block.addEventListener('keydown', onQuestionsBlockPressEnter(block));
+  }
+
 }());
